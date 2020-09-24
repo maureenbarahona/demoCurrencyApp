28ace1af80cb6fce6e0c744558d6c8f5436a2b8b
Maureen <maureenbarahona@gmail.com>
2020-09-24 16:39:15 -0600

    styles modification

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+# Dev Maureen Barahona - endpoints in https://exchangeratesapi.io/
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [ratesList, setRetesList] = useState([]);
+  const [base, setBase] = useState("USD");
 
   useEffect(() => {
-    console.log("test");
-    getRates();
+    getRates("USD");
   }, []);
 
-  const getRates = async () => {
+  const getRates = async (base) => {
     const res = await axios.get(
-      ` https://api.exchangeratesapi.io/latest?base=USD`
+      ` https://api.exchangeratesapi.io/latest?base=${base}`
     );
     const { rates } = res.data;
 
     const ratesTemp = [];
     for (const [symbol, rate] of Object.entries(rates)) {
-      console.log(rate);
       ratesTemp.push({ symbol, rate });
     }
     setRetesList(ratesTemp);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <body>
-        <select className="form-control form-control-lg">
-          {ratesList.map((d)=>(
-<option>
-{d.symbol}
-</option>
-          ))};
-        </select>
-        <ul className="list-group">
-          {ratesList.map((d) => 
-           <li className="list-group-item" key={d.symbol}>
-             {d.symbol}- {d.rate}</li> 
-          )}
-        </ul>
-      </body>
+    <div class="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h3>Currency convert</h3>
+          <br />
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Symbol :</label>
+              <select
+                className="form-control form-control-lg"
+                value={base}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  setBase(value);
+                  getRates(value);
+                }}
+              >
+                {ratesList.map((d) => (
+                  <option value={d.symbol}>{d.symbol}</option>
+                ))}
+                ;
+              </select>
+              <ul className="list-group">
+                {ratesList.map((d) => (
+                  <li className="list-group-item" key={d.symbol}>
+                    {d.symbol}- {d.rate}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
