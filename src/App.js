5b46fc71c582a1dda7d9ff01f74038462786e466
Maureen <maureenbarahona@gmail.com>
2020-09-24 16:07:29 -0600

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [ratesList, setRetesList] = useState([]);
+
+  useEffect(() => {
+    console.log("test");
+    getRates();
+  }, []);
+
+  const getRates = async () => {
+    const res = await axios.get(
+      ` https://api.exchangeratesapi.io/latest?base=USD`
+    );
+    const { rates } = res.data;
+
+    const ratesTemp = [];
+    for (const [symbol, rate] of Object.entries(rates)) {
+      console.log(rate);
+      ratesTemp.push({ symbol, rate });
+    }
+    setRetesList(ratesTemp);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,9 +34,19 @@ function App() {
         </p>
       </header>
       <body>
-        <select className="custom-select">
-          <option>12</option>
+        <select className="form-control form-control-lg">
+          {ratesList.map((d)=>(
+<option>
+{d.symbol}
+</option>
+          ))};
         </select>
+        <ul className="list-group">
+          {ratesList.map((d) => 
+           <li className="list-group-item" key={d.symbol}>
+             {d.symbol}- {d.rate}</li> 
+          )}
+        </ul>
       </body>
     </div>
   );
